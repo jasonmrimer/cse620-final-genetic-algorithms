b2da5ecd18c98a18295c3bb7dc02d55522e983fa
Engineer <jasonmrimer@gmail.com>
2021-12-09 10:58:21 -0700

    single crossover during crowding while using crossover in other techniques

--- sharing.m
+++ sharing.m
@@ -6,13 +6,14 @@ if option==1 || option==4
     
     for i = 1:sizepop
         for j=1:sizepop
-        
             d(i,j)=abs(newpop(i,stringlength+1)-newpop(j,stringlength+1));
+            
             if d(i,j)<sigmash & sigmash~=0
                 sh(j)=1-(d(i,j)/sigmash)^alpha;
             else
                 sh(j)=0;
             end
+            
         end
         newpop(i,stringlength+2)=newpop(i,stringlength+2)/sum(sh);
     end

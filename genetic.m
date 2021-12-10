5de81cc997e6ebdd6b34828e2de66e8322c48894
Engineer <jasonmrimer@gmail.com>
2021-12-09 23:06:10 -0700

    crossover now happens for all population members rather than two. works for sharing, broken for crowding

--- genetic.m
+++ genetic.m
@@ -3,7 +3,7 @@
 % option=4: for M4 function
 % sigmash, alpha are the parameters for sharing
 
-function [population, Fmax, Fmin, Faver, benchmark_function]=genetic( ...
+function [population, Fmax, Fmin, Faver, fitness_function]=genetic( ...
     population_size, ...
     chromosome_length, ...
     benchmark_domain_start, ...
@@ -17,30 +17,31 @@ function [population, Fmax, Fmin, Faver, benchmark_function]=genetic( ...
 
 
     [x, y] = initialize_graph_axes(option, benchmark_domain_start, benchmark_domain_end, handles);
-    benchmark_function = setup_benchmark(option);
-    plot(x,benchmark_function(x));
+    fitness_function = setup_benchmark(option);
+    plot(x,fitness_function(x));
     
     elites = [];
     Fmax = zeros(1, total_generations);
     Fmin = zeros(1, total_generations);
     Faver = zeros(1, total_generations);
     
-    population = initialise(population_size, chromosome_length, benchmark_domain_start, benchmark_domain_end, benchmark_function, option);
+    population = initialise(population_size, chromosome_length, benchmark_domain_start, benchmark_domain_end, fitness_function, option);
     plot_baseline_benchmark(option, population, chromosome_length);
 
     for j=1:total_generations
+        
+        population = crossover_all_pop(population, benchmark_domain_start, benchmark_domain_end, fitness_function, ...
+            chromosome_length, probability_of_crossover);
         if does_crowding==1
-            population=crowding(population, population_size, chromosome_length, benchmark_domain_start, benchmark_domain_end, benchmark_function, option);
+            population=crowding(population, population_size, chromosome_length, benchmark_domain_start, benchmark_domain_end, fitness_function, option);
         end
         
         if does_sharing==1
             population = sharing(population, population_size, chromosome_length, option, sigmash, alpha);
         end
         
-        population = calculate_fitness(population, population_size, chromosome_length, benchmark_function);
-        [Fmax(j), Fmin(j), Faver(j)] = capture_generation_fitness_measures(population, chromosome_length);
-        
-        
+        population = calculate_fitness(population, population_size, chromosome_length, fitness_function);
+        [Fmax(j), Fmin(j), Faver(j)] = capture_generation_fitness_measures(population, chromosome_length);  
     
         [ind1, ind2, wind1, wind2]=roulette(population, population_size, chromosome_length, option);%Selection methods
         
@@ -50,12 +51,12 @@ function [population, Fmax, Fmin, Faver, benchmark_function]=genetic( ...
         child1 = parent1;
         child2 = parent2;
         
-        if does_crowding~=1
-            [child1, child2]=crossover(parent1, parent2, benchmark_domain_start, benchmark_domain_end, option, benchmark_function, chromosome_length, probability_of_crossover);%crossover
-        end
+%         if does_crowding~=1
+%             [child1, child2]=crossover(parent1, parent2, benchmark_domain_start, benchmark_domain_end, option, fitness_function, chromosome_length, probability_of_crossover);%crossover
+%         end
         
         population = mutate_two_children(population, child1, child2, ...
-            benchmark_domain_start, benchmark_domain_end, benchmark_function, ...
+            benchmark_domain_start, benchmark_domain_end, fitness_function, ...
             chromosome_length, probability_of_mutation, ...
             wind1, wind2);
         
@@ -65,20 +66,10 @@ function [population, Fmax, Fmin, Faver, benchmark_function]=genetic( ...
         end
     end
     
-    if option==1 || option==4
-        xlabel('x');
-        ylabel('M(x)');
-        plot(population(:,chromosome_length+1),population(:,chromosome_length+2),'g*');
-        hleg1=legend('Function','Initial Optimum','Niche Points','Location','Southeast');
-    else
-        xlabel('x');
-        ylabel('M(x)');
-        plot3(population(:,2*chromosome_length+2),population(:,2*chromosome_length+1),population(:,2*chromosome_length+3),'g*');
-        title('Function and Population (initial,niched) plot')
-        hleg1=legend('Function','Initial Optimum','Niche Points','Location','SoutheastOutside');
-        
-    end
-
+    xlabel('x');
+    ylabel('M(x)');
+    plot(population(:,chromosome_length+1),population(:,chromosome_length+2),'g*');
+    hleg1=legend('Function','Initial Optimum','Niche Points','Location','Southeast');
     axes(handles.axes2);
     plot(Fmax), hold on, plot(Faver,'r-'), hold on, plot(Fmin,'g');
     xlabel('Generation')
@@ -156,3 +147,45 @@ function population = mutate_two_children(population, child1, child2, benchmark_
     population(wind1,:)=child1m;
     population(wind2,:)=child2m;
 end
+
+function new_population = crossover_all_pop(population, benchmark_domain_start, benchmark_domain_end, benchmark_function, ...
+            chromosome_length, probability_of_crossover)
+%     new_population = zeros(size(population,1),chromosome_length + 2, 'single');
+    new_population = [];
+    
+    while (parents_remain(population))
+        random_indeces = randperm(size(population, 1),2);
+        parent1 = population(random_indeces(1),:);
+        parent2 = population(random_indeces(2),:);
+
+        population = remove_parents(population, random_indeces);
+        
+
+        [child1, child2] = crossover( ...
+            parent1, parent2, ...
+            benchmark_domain_start, benchmark_domain_end, benchmark_function, ...
+            chromosome_length, probability_of_crossover);
+        new_population(end+1,:) = child1;
+        new_population(end+1,:) = child2;
+    end
+
+% pair off those indeces as parents
+% pop indeces, crossover parents
+    function result = parents_remain(population)
+        result = size(population, 1) > 1;
+    end
+    function population = remove_parents(population, indeces)
+        if (first_index_will_exceed_bounds(indeces))
+            population(indeces(1),:) = [];
+            population(indeces(2),:) = [];
+        else
+            population(indeces(2),:) = [];
+            population(indeces(1),:) = [];
+        end
+    end
+   
+    function result = first_index_will_exceed_bounds(indeces)
+        result = indeces(1) > indeces(2);
+    end
+end
+

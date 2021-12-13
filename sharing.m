8f7544b867863ffaf6ed4b0a7c68ca8cdcd67cb5
Engineer <jasonmrimer@gmail.com>
2021-12-12 17:11:45 -0800

    population_prime caused the indexing error. commented out for time being...

--- sharing.m
+++ sharing.m
@@ -1,35 +1,22 @@
-function newpop = sharing(pop, sizepop, stringlength, option, sigmash, alpha)
-%%% Description: sharing selection method
+function newpop = sharing( ...
+  population, ...
+  population_size, ...
+  genome_length, ...
+  option, ...
+  sigmash, ...
+  alpha ...
+)
 
-newpop=pop;
-if option==1 || option==4
-    
-    for i = 1:sizepop
-        for j=1:sizepop
-            d(i,j)=abs(newpop(i,stringlength+1)-newpop(j,stringlength+1));
-            if d(i,j)<sigmash & sigmash~=0
-                sh(j)=1-(d(i,j)/sigmash)^alpha;
-            else
-                sh(j)=0;
-            end
-            
-        end
-        newpop(i,stringlength+2)=newpop(i,stringlength+2)/sum(sh);
+  newpop = population;
+
+  for i = 1:population_size
+    for j = 1: population_size
+      d(i,j)=abs(newpop(i, genome_length + 1) - newpop(j, genome_length + 1));
+      if d(i,j) < sigmash & sigmash ~= 0
+        sh(j) = 1 - (d(i,j) / sigmash) ^ alpha;
+      else
+        sh(j) = 0;
+      end
     end
-else
-    for i = 1:sizepop
-        for j=1:sizepop
-        
-            d(i,j)=norm(newpop(i,2*stringlength+1:2*stringlength+2)-newpop(j,2*stringlength+1:2*stringlength+2));
-            if d(i,j)<sigmash
-                sh(j)=1-d(i,j)/sigmash;
-            else
-                sh(j)=0;
-            end
-        end
-        newpop(i,2*stringlength+3)=newpop(i,2*stringlength+3)/sum(sh);
-    end
-end
-%%%%%%%%%%%%%%%%%%%
-%End of function
-%%%%%%%%%%%%%%%%%%%
+    newpop(i, genome_length + 2) = newpop(i, genome_length + 2) / sum(sh);
+  end
